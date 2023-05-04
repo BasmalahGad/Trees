1f4f8b2e88af41ef01f76d8ee3241c5762dfca21
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2023-05-04 18:42:20 +0300

    Max Heap

---
+++ Implementations/Max Heap/MaxHeapFunctions.cpp
@@ -0,0 +1,6 @@
+// FCAI - Data Structures - 2023 - Assignment 2
+// Program Name: Max Heap
+// Last Modification Date:
+// Author3 and ID and Group: Basmala Mohamed Sayed Gad            ID: 20210090      Group: S25,26
+
+#include "MaxHeapHeader.h"
---
+++ Implementations/Max Heap/MaxHeapHeader.h
@@ -0,0 +1,12 @@
+// FCAI - Data Structures - 2023 - Assignment 2
+// Program Name: Max Heap
+// Last Modification Date:
+// Author3 and ID and Group: Basmala Mohamed Sayed Gad            ID: 20210090      Group: S25,26
+
+#ifndef MAX_HEAP_MAXHEAPHEADER_H
+#define MAX_HEAP_MAXHEAPHEADER_H
+
+#include <iostream>
+using namespace std;
+
+#endif //MAX_HEAP_MAXHEAPHEADER_H
