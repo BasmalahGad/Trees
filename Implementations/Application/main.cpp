7d640e9d39f76048601be0679daaff9bb3939fb9
BasmalahGad <101473200+BasmalahGad@users.noreply.github.com>
2023-05-04 18:08:40 +0300

    App

---
+++ Implementations/Application/main.cpp
@@ -0,0 +1,14 @@
+// FCAI - Data Structures - 2023 - Assignment 1
+// Program Name: Trees
+// Last Modification Date:
+// Author1 and ID and Group: Youssef Mohamed Salah Eldin Anwar    ID:20210483       Group: S25,26
+// Author2 and ID and Group: Abdelrahman Wael Mohammed Hanafy     ID: 20210490      Group: S25,26
+// Author3 and ID and Group: Basmala Mohamed Sayed Gad            ID: 20210090      Group: S25,26
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << ":)";
+    return 0;
+}
