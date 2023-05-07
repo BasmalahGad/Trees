4996d816eabebef169c505a256f3323a2e3560ab
youssefAnwar87 <yassoyosse@gmail.com>
2023-05-07 12:04:19 +0300

    added BST(Add stud)  : (

--- Implementations/BST/BSTFunctions.cpp
+++ Implementations/BST/BSTFunctions.cpp
@@ -4,3 +4,66 @@
 // Author and ID and Group: Youssef Mohamed Salah Eldin Anwar    ID:20210483       Group: S25,26
 
 #include "BSTHeader.h"
+void BST::add_student(string Name, int id, double GPA, string Department)
+{
+    Student s(id, GPA, Name, Department);
+
+    if (search(s))
+        return;
+
+    Node *n = new Node(s, NULL, NULL);
+    if (root == NULL)
+    {
+        root = n;
+        return;
+    }
+    Node *p = root;
+
+    // getting last node
+
+    while (true)
+    {
+        // p stands at the root
+        if (s.getId() > p->Student.getId())
+        {
+            if (p->right == NULL)
+                break;
+            p = p->right;
+        }
+        else
+        {
+            if (p->left == NULL)
+                break;
+            p = p->left;
+        }
+    }
+    // p stands at the last node
+    if (s.getId() > p->Student.getId())
+    {
+        p->right = n;
+    }
+    else
+    {
+        p->left = n;
+    }
+}
+bool BST::search(Student S)
+{
+    Node *p = root;
+    while (p != NULL)
+    {
+        if (p->Student.getId() == S.getId())
+            return true;
+        if (S.getId() > p->Student.getId())
+            p = p->right;
+        else
+            p = p->left;
+    }
+    return false;
+}
+void BST::remove_student(int id);
+void BST::search_student(int id)
+{
+}
+
+void BST::print();
--- Implementations/BST/BSTHeader.h
+++ Implementations/BST/BSTHeader.h
@@ -7,6 +7,45 @@
 #define BST_BSTHEADER_H
 
 #include <iostream>
+#include "../../Implementations/Application/Student.h"
 using namespace std;
+class Node
+{
+public:
+    Student Student;
+    Node *left;
+    Node *right;
+    Node()
+    {
+        Student.setDepartment("N/A");
+        Student.setId(0);
+        Student.setGPA(0.0);
+        Student.setName("None");
+        left = NULL;
+        right = NULL;
+    }
+    Node(int val, Node *l, Node *r)
+    {
+        value = val;
+        left = l;
+        right = r;
+    }
+};
+class BST
+{
+private:
+    Node *root;
 
-#endif //BST_BSTHEADER_H
+public:
+    BST()
+    {
+        root = NULL;
+    }
+    void add_student(string Name, int id, double GPA, string Department);
+    void remove_student(int id);
+    void search_student(int id);
+    bool search(Student s);
+    void print();
+};
+
+#endif // BST_BSTHEADER_H
